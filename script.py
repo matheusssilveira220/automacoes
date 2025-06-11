016341c99c87f8b5a23dc573c5e60d9374eeb0aa
Matheus Silveira <matheusilveira2048@hotmail.com>
2025-06-11 17:42:57 -0300

    Versão Final do Dia 11/06

--- script.py
+++ script.py
@@ -11,6 +11,7 @@ password = os.getenv("PASSWORD")
 #abrir navegador
 navegador = webdriver.Chrome()
 
+#login
 navegador.get("https://backoffice.omni.chat/#/retailer")
 navegador.maximize_window()
 time.sleep(1)
@@ -24,7 +25,29 @@ pyautogui.moveTo (x=150, y=500)
 time.sleep(1)
 pyautogui.click()  
 time.sleep(1)
-pyautogui.write (password)
+pyautogui.write(password)
 time.sleep(1)
 pyautogui.press("enter")
-time.sleep(10)
+time.sleep(5)
+
+#pesquisar cliente
+pesquisa = navegador.find_element(By.XPATH, '//*[@id="search-box"]')
+time.sleep(1)
+pesquisa.click()
+time.sleep(1)
+pesquisa.send_keys("ESKALA")
+time.sleep(1)
+pyautogui.press("enter")
+time.sleep(2)
+
+#acessar cliente
+selecionar_cliente = pesquisa.find_element(By.XPATH, '/html/body/app-root/app-menu/mat-sidenav-container/mat-sidenav-content/app-retailers/div/table/tbody/tr/td[1]')
+time.sleep(1)
+selecionar_cliente.click()
+time.sleep(1)
+
+#acessar tela de pagamentos
+pyautogui.moveTo (x=643, y=320)
+time.sleep(2)
+pyautogui.click()  
+time.sleep(5)
